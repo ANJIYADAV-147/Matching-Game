3cb2f9a266a45fe7e0642fc6375b8049d3d671ef
ANJIYADAV-147 <anjiyadav.kaitha@gmail.com>
2024-07-15 09:59:49 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import GameBoard from './components/GameBoard';
+import SuccessScreen from './components/SuccessScreen';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/game_start" element={<GameBoard />}/>
+        <Route path="/game_result" element={<SuccessScreen/>}/>
+      </Routes>
+    </Router>
   );
 }
 
